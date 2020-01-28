b90ea74cefccd86e9f468fef3a1343110c4b4ebe
Kaylin Devchand <45614089+kaylindevchand@users.noreply.github.com>
2020-01-28 17:36:18 -0500

    Sync object and string

--- string.h
+++ string.h
@@ -1,125 +1,49 @@
+//lang::CwC
 #pragma once
+
 #include "object.h"
-#include <stdlib.h>
-#include <string.h>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio> 
 
 /**
- * @brief String class implementation. A String is an Object. 
- * 
- */
-class String: public Object {
-    
-    public:
+ * An immutable String class representing a char*
+ * author: chasebish */
+class String : public Object {
+public:
+  /** CONSTRUCTORS & DESTRUCTORS **/
 
-        /**
-         * @brief we have two local variables, size, and str_, which represent the string this String object holds and its size
-         * 
-         */
-        size_t size;
-        char* str_;
+  /* Creates a String copying s */
+  String(const char* s);
 
-        /**
-         * @brief Construct a new empty String object
-         * 
-         */
-        String() {
-            size = 0;
-            str_ = nullptr;
-        }
+  /* Copies a String copying the value from s */
+  String(String* const s);
 
-        /**
-         * @brief Construct a new String object that holds the given string str
-         * 
-         * @param str - the string this object will hold
-         */
-        String(const char* str) {
-            size = strlen(str);
-            str_ = new char[size + 1];
-            for(size_t i = 0; i < size; i++) {
-                str_[i] = str[i];
-            }
+  /* Clears String from memory */
+  ~String();
 
-            str_[size] = '\0';
-        }
 
-        /**
-         * @brief Destroy the String object
-         * 
-         */
-        ~String() {
-            size = 0;
-            delete[] str_;
-        }
+  /** INHERITED METHODS **/
 
-        /**
-         * @brief concatenates the this String's str_ to the other String's str_ and returns a new String object holding 
-         * the concatenated strings
-         * 
-         * @param other - the other String
-         * @return String* 
-         */
-        String* concat(String* other) {
+  /* Inherited from Object, generates a hash for a String */
+  size_t hash();
 
-            char* new_str_ = new char[size + other->size - 1];
+  /* Inherited from Object, checks equality between an String and an Object */
+  bool equals(Object* const obj);
 
-            for(size_t i = 0; i < size; i++) {
-                new_str_[i] = str_[i];
-            }
 
-            for(size_t i = size; i < size + other->size; i++) {
-                new_str_[i] = other->str_[i-size];
-            }
+  /** STRING METHODS **/
+  
+  /** Compares strings based on alphabetical order
+   * < 0 -> this String is less than String s
+   * = 0 -> this String is equal to String s
+   * > 0 -> this String is greater than String s
+   */
+  int cmp(String* const s);
 
-            return new String(new_str_);
-        }
+  /* Creates a new String by combining two existing Strings */
+  String* concat(String* const s);
 
-        /**
-         * @brief determines whethere this String's str_ is lexographically higher than the other's str_ 
-         * 
-         * @param other - the other String
-         * @return true - if this is lexographically greater
-         * @return false - otherwise
-         */
-        bool lex_greater(String* other) {
-            for(size_t i = 0; i < std::min(size, other->size); i++) {
-                if(str_[i] - '0' < other->str_[i] - '0') {
-                    return false;
-                }
-                else if(str_[i] - '0' > other->str_[i] - '0') {
-                    return true;
-                }
-            }
-            return size > other->size;
-        }
-
-        /**
-         * @brief overriding the the parent's hash method - this simple implementation adds the ASCII values of the chars
-         * in this String. We start with a prime number and multiply each ASCII value by 7 a prime number) which generates
-         * more unique hashes and avoids collisions. We also multiply by i in order to make sure all permutations of a string 
-         * dont have the same hash code. 
-         * 
-         * @return size_t 
-         */
-        virtual size_t hash() {
-            size_t sum = 7;
-            for(size_t i = 0; i < size; i++) {
-                sum += str_[i] * 7 * (i + 1);
-            }
-
-            return sum;
-        }
-
-        /**
-         * @brief overriding the parent's equals method - we use casting and then compare
-         * 
-         * @param other - another Object
-         * @return true - if they're equal strings
-         * @return false - otherwise
-         */
-        virtual bool equals(Object* other) {
-
-            String* other_str = dynamic_cast <String *> (other);
-
-            return (other_str && strcmp(str_, other_str->str_) == 0);
-        }
+  /* Returns the current length of the String */
+  size_t size();
 };
